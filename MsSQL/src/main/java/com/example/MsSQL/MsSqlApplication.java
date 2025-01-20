137a5f405777d3052c21d01a25abd22401f58377
WalterSanchezIng <waltersanchez.ing@gmail.com>
2025-01-19 20:05:02 -0500

    comit

---
+++ MsSQL/src/main/java/com/example/MsSQL/MsSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.MsSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSqlApplication.class, args);
+	}
+
+}
---
+++ MsSQL/src/test/java/com/example/MsSQL/MsSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MsSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
